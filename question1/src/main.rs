8758b29fbd817eb36bc5b797cca85d87f95565fe
masahiko kawai <masahiko_kawai@fablic.co.jp>
2021-02-13 17:32:30 +0900

    add answer question1

--- question1/src/main.rs
+++ question1/src/main.rs
@@ -1,7 +1,7 @@
 // println!でstr1が表示できるように4行目を修正してください
 fn main() {
     let str1 = String::from("hoge");
-    let str2 = str1;
+    let _str2 = str1.to_string(); // もしくはコメントアウトでstr1の借用を消す
 
     println!("Hello, world! {}", str1);
 }
