1da409436e72a635fc06508489e8b712d59e4a95
masahiko kawai <masahiko_kawai@fablic.co.jp>
2021-02-13 17:46:13 +0900

    add answer question4

--- question4/src/main.rs
+++ question4/src/main.rs
@@ -1,7 +1,7 @@
 // 7行目のscoresが表示されるように修正してください
 fn main() {
     let scores = vec![100, 92, 84, 75, 98, 81];
-    for score in scores.into_iter() {
+    for score in scores.iter() { // into_iter() -> iter()
         println!("{}", score);
     }
     println!("{:?}", scores);
