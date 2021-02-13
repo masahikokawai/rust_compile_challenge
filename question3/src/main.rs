657833b4fa60f424343ee5c02a9969413951a763
masahiko kawai <masahiko_kawai@fablic.co.jp>
2021-02-13 17:43:04 +0900

    add answer question3

--- question3/src/main.rs
+++ question3/src/main.rs
@@ -1,6 +1,6 @@
 // countの値が出力されるように1箇所修正してください
 fn main() {
-    let count = 0u32;
+    let mut count = 0u32; // mut をつけてcountを可変にする
     while count < 10 {
         count += 1;
         println!("count={}", count);
